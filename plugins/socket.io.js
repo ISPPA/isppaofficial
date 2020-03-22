b000a071f06082f0355d737e237c261e602362e7
consuman <nicolai@consuman.de>
2020-03-22 14:54:55 +0100

    small build fix

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 import pkg from '~/package';
 
-const socket = io.connect(`wss://${pkg.backend}/`, { secure: true, rejectUnauthorized: false });
+const socket = io.connect(`wss://${pkg.backend}/`);
 
 export default socket;
