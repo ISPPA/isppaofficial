5e47d31ab819f74a5b4bd60bfd208e5f29074dde
Thibaut Rey <thibautrey@me.com>
2020-03-22 13:25:49 +0100

    Fix issues with Websockets point to unsecure

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -1,6 +1,6 @@
-import io from 'socket.io-client'
-import pkg from '~/package'
+import io from 'socket.io-client';
+import pkg from '~/package';
 
-const socket = io.connect(`ws://${pkg.backend}/`)
+const socket = io.connect(`wss://${pkg.backend}/`, { secure: true, rejectUnauthorized: false });
 
-export default socket
+export default socket;
